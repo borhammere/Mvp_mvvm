b92df84df2684b3cd9157fed2f334946d1c415a9
borhammere <borhammere@gmail.com>
2022-03-31 22:14:12 +0300

    base mvp implementation

---
+++ app/src/main/java/com/example/mvp_mvvm/LoginContract.kt
@@ -0,0 +1,26 @@
+package com.example.mvp_mvvm
+
+import androidx.annotation.MainThread
+
+class LoginContract {
+
+    interface View {
+        @MainThread
+        fun setSuccess()
+
+        @MainThread
+        fun setError(error: String)
+
+        @MainThread
+        fun showProgress()
+
+        @MainThread
+        fun hideProgress()
+    }
+
+    interface Presenter {
+        fun onAttach(view: View)
+        fun onLogin(login: String, password: String)
+        fun onCredentialsChanged()
+    }
+}
--- app/src/main/java/com/example/mvp_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvp_mvvm/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.example.mvp_mvvm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.annotation.MainThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.mvp_mvvm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), LoginContract.View {
+    private lateinit var binding: ActivityMainBinding
+    private var presenter: LoginContract.Presenter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        presenter = restorePresenter()
+        presenter?.onAttach(this)
+
+        binding.loginButton.setOnClickListener {
+            presenter?.onLogin(
+                binding.loginEditText.text.toString(),
+                binding.passwordEditText.text.toString()
+            )
+        }
+    }
+
+    private fun restorePresenter(): LoginPresenter {
+        val presenter = lastCustomNonConfigurationInstance as? LoginPresenter
+        return presenter ?: LoginPresenter()
+    }
+
+    override fun onRetainCustomNonConfigurationInstance(): Any? {
+        return presenter
+    }
+
+    @MainThread
+    override fun setSuccess() {
+        binding.loginButton.isVisible = false
+        binding.loginEditText.isVisible = false
+        binding.passwordEditText.isVisible = false
+        binding.root.setBackgroundColor(Color.GREEN)
+    }
+
+    @MainThread
+    override fun setError(error: String) {
+        Toast.makeText(this, "ERROR $error", Toast.LENGTH_SHORT).show()
+    }
+
+    @MainThread
+    override fun showProgress() {
+        binding.loginButton.isEnabled = false
+        hideKeyboard(this)
+    }
+
+    @MainThread
+    override fun hideProgress() {
+        binding.loginButton.isEnabled = true
+    }
+
+    private fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view: View? = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
